7b06e34dec3f8bbb7294439bf10c9656a89f4904
ajacobs055 <adamrobot04@gmail.com>
2026-02-20 18:08:43 -0800

    testing mavlink and liftoff

---
+++ simulation-manual-movement/main.py
@@ -0,0 +1,33 @@
+
+from pymavlink import mavutil
+from drone import Drone
+import time
+
+drone = Drone()
+
+drone.connect()
+
+drone.set_mode("NOGPS") #its 20 rn so its nogps mode
+
+drone.arm()
+
+drone.test()
+time.sleep(3)
+drone.test2()
+
+time.sleep(3)
+
+drone.set_mode("LAND")
+
+drone.dearm()
+# while True:
+#     drone.move(5, 0, -2)
+#     time.sleep(1)
+#     # drone.move(5, 5, -2)
+#     # time.sleep(4)
+#     # drone.move(0, 5, -2)
+#     # time.sleep(4)
+#     # drone.move(0, 0, -2)
+#     # time.sleep(4)
+#    # drone.printMav()
+#     print("the devil has angel wings")
